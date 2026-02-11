8c9f721ce50052a00ededb9a5d974151faa0bc87
Shubham Balavant Randive <srandive245@gmail.com>
2026-02-11 11:32:50 +0530

    first commit

---
+++ DemoScript.py
@@ -0,0 +1,7 @@
+# Here is the place we will write the python code
+# print("Hello");
+
+num1 = 10;
+num2 = 20;
+Sum = num1 + num2;
+print(Sum)
---
+++ ListComprehension.py
@@ -0,0 +1,28 @@
+# squeres = []
+# for i in range(1, 11):
+#     squeres.append(i**2)
+#
+# print(squeres)
+
+# List Comprehension
+# squeres = [i**2 for i in range(1, 11)]
+
+# print(squeres)
+
+# def is_prime(n):
+#     if n <= 1:
+#         return False
+#     for i in range(2, int(n**0.5) + 1):
+#         if n % i == 0:
+#             return False
+#     return True
+#
+# primes = [x for x in range(50) if is_prime(x)]
+# print(primes)
+
+lst = ['Shubham', 'Bhag', 'Amit', 'Gopal', 'Ream']
+lst.sort()
+print(lst)
+lst.reverse()
+
+print(lst)
